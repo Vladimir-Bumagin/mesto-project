2633bec06d81a4f9630ee2a29c6a1c54d75664ed
Vladimir-Bumagin <vl.bumagin@yandex.ru>
2022-01-29 15:23:29 +0300

    comments corrected

--- script.js
+++ script.js
@@ -27,10 +27,8 @@ const initialCards = [
 const userEditButton = document.querySelector(".profile__edit-button");
 const addImageButton = document.querySelector(".profile__add-images-button");
 const watchImageCloseButton = document.querySelector("#watch-image-closer");
-const popUpContainer = document.querySelector(".popup__container");
 const userForm = document.querySelector(".user-info-form");
 const userSubmitButton = userForm.querySelector(".user-info-form__button-save");
-const popUp = document.querySelector(".popup");
 const userName = userForm.querySelector("#user-name");
 const userCareer = userForm.querySelector("#career");
 const profileName = document.querySelector(".profile__title");
@@ -47,14 +45,29 @@ const placeName = document.querySelector("#place-name");
 const imageLink = document.querySelector("#place-link");
 const popUpUserCloseButton = editProfile.querySelector("#user-close-button");
 const photoCards = document.querySelector(".photo-cards");
+const photoCard = document.querySelector(".photo-card");
+const photoCardTemplate = document.querySelector("#photo-card").content;
+
 
 userEditButton.addEventListener("click", openPopUpUser);
 
 addImageButton.addEventListener("click", openPopUpImage);
 
-userForm.addEventListener("submit", userFormSubmitHandler);
+userForm.addEventListener("submit", handleFormSubmit);
 
-imageForm.addEventListener("submit", isAddPhoto);
+imageForm.addEventListener("submit", addPhoto);
+
+popUpUserCloseButton.addEventListener("click", () => {
+  closePopUp(editProfile);
+});
+
+popUpAddImageCloseButton.addEventListener("click", () => {
+  closePopUp(addImages);
+});
+
+watchImageCloseButton.addEventListener("click", () => {
+  closePopUp(watchImages);
+});
 
 function closePopUp(popUp) {
   popUp.classList.remove("popup_opened");
@@ -69,94 +82,63 @@ function openPopUpWatchImage(photo) {
   watchImage.alt = photo.alt;
   imageCaption.textContent = photo.alt;
   openPopUp(watchImages);
-  watchImageCloseButton.addEventListener("click", () => {
-    closePopUp(watchImages);
-  });
 }
 
 function openPopUpUser() {
   openPopUp(editProfile);
   userName.value = profileName.textContent;
   userCareer.value = profileCareer.textContent;
-  popUpUserCloseButton.addEventListener("click", () => {
-    closePopUp(editProfile);
-  });
 }
 
 function openPopUpImage() {
   openPopUp(addImages);
   placeName.value = "";
   imageLink.value = "";
-  popUpAddImageCloseButton.addEventListener("click", () => {
-    closePopUp(addImages);
-  });
 }
 
-function userFormSubmitHandler(evt) {
+function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = userName.value;
   profileCareer.textContent = userCareer.value;
-  editProfile.classList.remove("popup_opened");
+  closePopUp(editProfile);
 }
 
-//Функция удаления картинки
 
-photoCards.addEventListener("click", function (event) {
-  const deleteButton = event.target.closest(".photo-card__delete-button");
-  if (!deleteButton) return;
-  deleteButton.parentElement.parentElement.remove();
-});
+//Создание карточки по шаблону
+function createCard(item, itemTitle) {
+  const photoCardElement = photoCardTemplate
+    .querySelector(".photo-card")
+    .cloneNode(true);
+  const deleteButton = photoCardElement.querySelector(".photo-card__delete-button");
+  const likeButton = photoCardElement.querySelector(".photo-card__like-button");
+  const cardImage = photoCardElement.querySelector(".photo-card__image");
+  const cardTitle = photoCardElement.querySelector(".photo-card__title");
+  cardImage.src = item;
+  cardImage.alt = itemTitle;
+  cardTitle.textContent = itemTitle;
+  cardImage.addEventListener("click", () => 
+    openPopUpWatchImage(cardImage)
+  );
+  deleteButton.addEventListener("click", () => photoCardElement.remove());
+  likeButton.addEventListener("click", () =>
+    likeButton.classList.toggle("photo-card__like-button_active")
+  );
+  return photoCardElement;
+}
+// Заполняем данные фотокарточки из template через форму
 
-//Функция "Лайк"
-
-photoCards.onclick = function (event) {
-  let likeButton = event.target.closest(".photo-card__like-button");
-  if (!likeButton) return;
-  likeButton.classList.toggle("photo-card__like-button_active");
-};
+function addPhoto(evt) {
+  evt.preventDefault();
+  photoCards.prepend(createCard(imageLink.value, placeName.value));
+  closePopUp(addImages);
+  placeName.value = "";
+  imageLink.value = "";
+}
 
 //Добавление фотокарточек из начального массива
 
-const photoCardTemplate = document.querySelector("#photo-card").content;
-const initialElements = [];
-
-for (let i = 0; i < initialCards.length; i++) {
-  const photoCardElement = photoCardTemplate
-    .querySelector(".photo-card")
-    .cloneNode(true);
-  photoCardElement.querySelector(".photo-card__image").src =
-    initialCards[i].link;
-  photoCardElement.querySelector(".photo-card__image").alt =
-    initialCards[i].name;
-  photoCardElement.querySelector(".photo-card__title").textContent =
-    initialCards[i].name;
-  initialElements[i] = photoCardElement;
-  const image = initialElements[i].querySelector(".photo-card__image");
-  photoCards.append(initialElements[i]);
-  image.addEventListener("click", () => {
-    openPopUpWatchImage(image);
-  });
-}
-
-//Заполняем данные фотокарточки из template через форму
-
-function isAddPhoto(evt) {
-  const photoCardElement = photoCardTemplate
-    .querySelector(".photo-card")
-    .cloneNode(true);
-  evt.preventDefault();
-
-  photoCardElement.querySelector(".photo-card__image").src = imageLink.value;
-  photoCardElement.querySelector(".photo-card__image").alt = placeName.value;
-  photoCardElement.querySelector(".photo-card__title").textContent =
-    placeName.value;
-  addImages.classList.remove("popup_opened");
-  photoCards.prepend(photoCardElement);
-  placeName.value = "";
-  imageLink.value = "";
-  const image = photoCardElement.querySelector(".photo-card__image");
-
-  image.addEventListener("click", () => {
-    openPopUpWatchImage(image);
-  });
-}
+initialCards.forEach(function (item) {
+  const cardLink = item.link;
+  const cardName = item.name;
+  photoCards.append(createCard(cardLink, cardName));
+});
